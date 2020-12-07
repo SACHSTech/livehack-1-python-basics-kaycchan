63643df7e74335a76c84ebbcfb2a40c095ea486e
kaycchan <kayley.chan23@ycdsbk12.ca>
2020-12-07 18:36:53 +0000

    completed

--- problem1.py
+++ problem1.py
@@ -1,1 +1,19 @@
+'''
+-------------------------------------------------------------------------------
+Name:		problem1.py
+Purpose:	convert degrees celsius to degrees fahrenheit
 
+Author:	Chan.K
+
+Created:	07/12/2020
+------------------------------------------------------------------------------
+'''
+
+# get degrees in celsius
+celsius = float(input("Enter degrees in Celsius: "))
+
+# convert celsius to fahrenheit
+fahrenheit = 9/5*celsius+32
+
+# final statement of celsius to fahrenheit
+print (str(celsius) + " degrees Celsius is " + str(fahrenheit) + " degrees Fahrenheit.")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,28 @@
+'''
+-------------------------------------------------------------------------------
+Name:		problem2.py
+Purpose:	determine the number of chicken pieces per student, and number of chicken pieces for Mr. Fabroa
 
+Author:	Chan.K
+
+Created:	07/12/2020
+------------------------------------------------------------------------------
+'''
+
+# input number of chicken pieces
+number_of_chicken = float(input("Enter number of total chicken pieces: "))
+
+# input number of students
+number_of_students = float(input("Enter number of students: "))
+
+# number of mr. fabroas
+mr_fabroa = 1
+
+# determine the amount of chicken pieces per student
+chicken_per_student = number_of_chicken//number_of_students
+
+# determine the amount of chicken for mr.fabroa
+fabroa_chicken = number_of_chicken%number_of_students
+
+# final statement of chicken per student and mr.fabroa
+print ("If there are " + str(number_of_students) + " students in the class, and " + str(number_of_chicken) + " piece(s) of chicken, each student will get " + str(chicken_per_student) + " piece(s) of chicken, and Mr.Fabroa will get " + str(fabroa_chicken) + " piece(s) of chicken.")
